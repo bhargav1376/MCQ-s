eaf0bfdcc3aa0013bdfda55b5055481a8e448961
bhargav1376 <bhargav26394588@gmail.com>
2026-05-19 12:52:02 +0530

    'removea'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import { Analytics } from "@vercel/analytics/react";
 import Dashboard from "./Questions/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
-      <Analytics />
     </div>
   );
 }
